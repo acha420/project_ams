52ee7c90937676a6a7bb14231853fb8daa2611aa
acha420 <patelachal42@gmail.com>
2026-02-27 10:54:24 +0530

    Added authentication system and upload route configuration

--- fav_show/urls.py
+++ fav_show/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('upload/', views.upload, name='upload'),
 ]
--- fav_show/views.py
+++ fav_show/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    return render(request, 'login.html')
+
+@login_required
+def upload(request):
+    return render(request, 'upload.html')
